e0dd7205fc389b42bebdb1c913c41af2159f52db
Alastair Cottier <cottiera@shaw.ca>
2025-11-06 23:01:42 -0700

    :sparkles: add mock layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,17 @@
-// Top Nav
+import TopNav from "@/components/Nav";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <TopNav />
+        {children}
+      </body>
+    </html>
+  );
+}
